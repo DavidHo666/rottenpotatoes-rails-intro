595375f20b8176fb3c510393a8bf9009f9f70e88
Dawei He <dh3027@columbia.edu>
2022-10-01 14:54:36 -0400

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,15 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+
+    if params[:ratings]
+      @ratings_to_show = params[:ratings].keys
+    else
+      @ratings_to_show = []
+    end
+
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
